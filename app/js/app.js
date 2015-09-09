82b1c4451e2d21932381a267e25a41f4b3aef92c
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-09-09 20:00:03 +0300

    tring nested view

--- app/js/app.js
+++ app/js/app.js
@@ -3,14 +3,35 @@
 var App = angular.module('App', ['ui.router', 'ngAnimate', 'ngAria', 'ngMaterial']);
 
 App.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise("/")
+  $urlRouterProvider.otherwise("/technology")
       
       $stateProvider
-        .state('Home', {
+        .state('home', {
             url: "/",
             controller : 'MainCtrl',
             templateUrl: "/partials/main.html"
         })
+        .state('home.technology', {
+              url: "/technology",
+              templateUrl: "/partials/technology.html",
+              controller:"TechCtrl"
+          })
+        .state('home.sports', {
+              url: "/sports",
+              templateUrl: "/partials/sports.html",
+              controller:"SportsCtrl"
+          })
+        .state('home.science', {
+              url: "/science",
+              templateUrl: "/partials/science.html",
+              controller:"ScienceCtrl"
+          })
+        .state('home.entreprenuership', {
+              url: "/entreprenuership",
+              templateUrl: "/partials/entreprenuership.html",
+              controller:"EntreprenuershipCtrl"
+          });
+          
 });
 
 App.controller('MainCtrl', function($scope) {
@@ -23,7 +44,18 @@ App.controller('MainCtrl', function($scope) {
   ];
 });
 
+App.controller('TechCtrl', function($scope) {
 
+});
 
+App.controller('EntreprenuershipCtrl', function($scope) {
+
+});
+App.controller('ScienceCtrl', function($scope) {
+
+});
+App.controller('SportsCtrl', function($scope) {
+
+});
 
 
