25a492a0e901ec453b0f5ac9d56f180ae1d5afe4
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-09-13 23:49:59 +0300

    starting scrapping

---
+++ scraping.py
@@ -0,0 +1,13 @@
+import numpy as np
+import urllib2
+import bs4 #this is beautiful soup
+import time
+import operator
+import socket
+import cPickle
+import re # regular expressions
+
+import pandas as pd
+
+def scrap():
+	pass
